d5f8b45a61b24e8d0a32bc3766bae3838d5cf5c0
Renga Praveen Kumar <rengapraveen.kumar@toolagen.com>
2025-10-31 17:23:35 +0530

    fix: Image src changed

--- lib/presentation/screens/key_holder/data.dart
+++ lib/presentation/screens/key_holder/data.dart
@@ -1,37 +1,45 @@
 final mockedData = [
-    {
-      "id": 1,
-      "name": "Home Keys",
-      "imgUrl": "https://picsum.photos/id/1/200/300"
-    },
-    {
-      "id": 2,
-      "name": "Office Keys",
-      "imgUrl": "https://picsum.photos/id/2/200/300"
-    },
-    {
-      "id": 3,
-      "name": "Car Keys",
-      "imgUrl": "https://picsum.photos/id/3/200/300"
-    },
-    {
-      "id": 4,
-      "name": "Shed Keys",
-      "imgUrl": "https://picsum.photos/id/4/200/300"
-    },
-    {
-      "id": 5,
-      "name": "Mailbox Keys",
-      "imgUrl": "https://picsum.photos/id/5/200/300"
-    },
-    {
-      "id": 6,
-      "name": "Safe Keys",
-      "imgUrl": "https://picsum.photos/id/6/200/300"
-    },
-    {
-      "id": 7,
-      "name": "Storage Keys",
-      "imgUrl": "https://picsum.photos/id/7/200/300"
-    }
-  ];
+  {
+    "id": 1,
+    "name": "Home Keys",
+    // "imgUrl": "https://picsum.photos/id/1/200/300"
+    "imgUrl":
+        "https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=773"
+  },
+  {
+    "id": 2,
+    "name": "Office Keys",
+    "imgUrl":
+        "https://images.unsplash.com/photo-1654944932733-bca31b703dd7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=435"
+  },
+  {
+    "id": 3,
+    "name": "Car Keys",
+    "imgUrl":
+        "https://images.unsplash.com/photo-1671719367451-7bf05ae9549c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=871"
+  },
+  {
+    "id": 4,
+    "name": "Shed Keys",
+    "imgUrl":
+        "https://images.unsplash.com/photo-1609587415882-97552f39c6c2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=327"
+  },
+  {
+    "id": 5,
+    "name": "Mailbox Keys",
+    "imgUrl":
+        "https://plus.unsplash.com/premium_photo-1679768606018-7ac0b7583957?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=870"
+  },
+  {
+    "id": 6,
+    "name": "Safe Keys",
+    "imgUrl":
+        "https://images.unsplash.com/photo-1721036265023-6a794e2e249f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=435"
+  },
+  {
+    "id": 7,
+    "name": "Storage Keys",
+    "imgUrl":
+        "https://images.unsplash.com/photo-1687075430355-ed8df51c1670?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=774"
+  }
+];
